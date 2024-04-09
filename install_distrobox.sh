023d5ae1ede92a4c48cb273459010c1e50f53415
evan-poster <evan.poster@gmail.com>
2024-04-09 10:38:29 -0400

    Added commands for installing/uninstalling distrobox directly

---
+++ install_distrobox.sh
@@ -0,0 +1,4 @@
+# Installs distrobox directly from GitHub
+# Does NOT use system's package manager
+
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --prefix ~/.local
---
+++ uninstall_distrobox.sh
@@ -0,0 +1,4 @@
+# Uninstalls distrobox directly from GitHub
+# Does NOT use system's package manager
+
+curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/uninstall | sudo sh
