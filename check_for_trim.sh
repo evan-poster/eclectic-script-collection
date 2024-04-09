e6795f68051dac03e4d49db648445a31cab6f8b4
evan-poster <evan.poster@gmail.com>
2024-04-09 10:37:48 -0400

    cleaned echo on check_for_trim.sh

--- check_for_trim.sh
+++ check_for_trim.sh
@@ -3,6 +3,6 @@
 # Helper script that checks if your SSD supports TRIM.
 # Generally unnecessary once remembered, but I don't do it often enough to do so.
 
-echo -e "Checking if TRIM is supported. If DISC-ALN and DISC-ZERO are all zeroes, TRIM is supported.\n"
+echo "Checking if TRIM is supported. If DISC-ALN and DISC-ZERO are all zeroes, TRIM is supported.\n"
 
 lsblk --discard
